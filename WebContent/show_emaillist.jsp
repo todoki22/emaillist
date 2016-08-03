870dbe3bc984c06305d82faafc2087aa60b2a6e7
kickscar <kickscar@gmail.com>
2016-08-03 10:20:51 +0900

    show_emaillist.jsp 완성

--- WebContent/show_emaillist.jsp
+++ WebContent/show_emaillist.jsp
@@ -1,4 +1,11 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@page import="java.util.List"%>
+<%@page import="kr.ac.sungkyul.emaillist.dao.EmailListDao"%>
+<%@page import="kr.ac.sungkyul.emaillist.vo.EmailListVo"%>
+<%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+	EmailListDao dao = new EmailListDao();
+	List<EmailListVo> list = dao.getList();
+%>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -7,23 +14,32 @@
 <body>
 	<h1>메일 리스트에 가입되었습니다.</h1>
 	<p>입력한 정보 내역입니다.</p>
-	<!-- 메일정보 리스트 -->
-	<%-- list에서 하나씩 빼서 테이블를 채운다--%>
+
+	<%
+		for( EmailListVo vo : list ) {
+	%>
 	<table border="1" cellpadding="5" cellspacing="2">
 		<tr>
 			<td align=right>First name: </td>
-			<td>안</td>
+			<td><%=vo.getLastName() %></td>
 		</tr>
 		<tr>
 			<td align=right width="110">Last name: </td>
-			<td width="110">대혁</td>
+			<td width="110"><%=vo.getFirstName() %></td>
 		</tr>
 		<tr>
 			<td align=right>Email address: </td>
-			<td>kickscar@gmail.com</td>
+			<td><%=vo.getEmail() %></td>
+		</tr>
+		<tr>
+			<td align=right>Register Date: </td>
+			<td><%=vo.getRegDate() %></td>
 		</tr>
 	</table>
 	<br>
+	<%
+		}
+	%>
 	<p>
 		추가메일 등록
 	</p>
---
+++ src/kr/ac/sungkyul/emaillist/dao/EmailListDao.java
@@ -0,0 +1,78 @@
+package kr.ac.sungkyul.emaillist.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.ac.sungkyul.emaillist.vo.EmailListVo;
+
+public class EmailListDao {
+	
+	public List<EmailListVo> getList() {
+		List<EmailListVo> list = new ArrayList<EmailListVo>();
+		
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName( "oracle.jdbc.driver.OracleDriver" );
+			
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			
+			stmt = conn.createStatement();
+			
+			String sql = 
+				"   select no, " +
+			    "          first_name, " + 
+				"          last_name, " + 
+			    "          email, " + 
+				"          to_char(reg_date, 'yyyy-mm-dd')" +
+			    "     from emaillist" +
+				" order by reg_date desc";
+			
+			rs = stmt.executeQuery(sql);
+			while( rs.next() ) {
+				Long no = rs.getLong( 1 );
+				String firstName = rs.getString( 2 );
+				String lastName = rs.getString( 3 );
+				String email = rs.getString( 4 );
+				String regDate = rs.getString( 5 );
+				
+				EmailListVo vo = new EmailListVo();
+				vo.setNo(no);
+				vo.setFirstName(firstName);
+				vo.setLastName(lastName);
+				vo.setEmail(email);
+				vo.setRegDate(regDate);
+				
+				list.add( vo );
+			}
+			
+		} catch( ClassNotFoundException e ) {
+			e.printStackTrace();
+		} catch( SQLException e ) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if( rs != null ) {
+					rs.close();
+				}
+				if( stmt != null ) {
+					stmt.close();
+				}
+				if( conn != null ) {
+					conn.close();
+				}
+			} catch( SQLException e ) {
+				e.printStackTrace();
+			}
+		}
+
+		return list;
+	}
+}
