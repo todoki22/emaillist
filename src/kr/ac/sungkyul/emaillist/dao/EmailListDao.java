44b670e3b686d10052f6f47f9fe31bad084cb0a3
kickscar <kickscar@gmail.com>
2016-08-03 13:06:17 +0900

    DAO refactoring

--- src/kr/ac/sungkyul/emaillist/dao/EmailListDao.java
+++ src/kr/ac/sungkyul/emaillist/dao/EmailListDao.java
@@ -13,6 +13,19 @@ import kr.ac.sungkyul.emaillist.vo.EmailListVo;
 
 public class EmailListDao {
 	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null ;
+		try {
+			Class.forName( "oracle.jdbc.driver.OracleDriver" );
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch( ClassNotFoundException e ) {
+			e.printStackTrace();
+		} 
+		
+		return conn;
+	}
+	
 	public boolean insert( EmailListVo vo ) {
 		
 		Connection conn = null;
@@ -20,10 +33,7 @@ public class EmailListDao {
 		int count = 0;
 		
 		try {
-			Class.forName( "oracle.jdbc.driver.OracleDriver" );
-			
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			conn = getConnection();
 			
 			String sql = 
 " insert into emaillist values(seq_emaillist.nextval, ?, ?, ?, sysdate)";
@@ -35,9 +45,6 @@ public class EmailListDao {
 			
 			count = pstmt.executeUpdate();
 			
-		} catch( ClassNotFoundException e ) {
-			e.printStackTrace();
-			return false;
 		} catch( SQLException e ) {
 			e.printStackTrace();
 			return false;
@@ -65,11 +72,7 @@ public class EmailListDao {
 		Statement stmt = null;
 		ResultSet rs = null;
 		try {
-			Class.forName( "oracle.jdbc.driver.OracleDriver" );
-			
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-			
+			conn = getConnection();
 			stmt = conn.createStatement();
 			
 			String sql = 
@@ -99,8 +102,6 @@ public class EmailListDao {
 				list.add( vo );
 			}
 			
-		} catch( ClassNotFoundException e ) {
-			e.printStackTrace();
 		} catch( SQLException e ) {
 			e.printStackTrace();
 		} finally {
